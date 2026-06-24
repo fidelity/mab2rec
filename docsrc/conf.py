74ae701499052284067ea4bab62bc893785f0a34
skadio <serdar.kadioglu@fmr.com>
2026-06-24 14:10:58 -0400

    update

--- docsrc/conf.py
+++ docsrc/conf.py
@@ -53,7 +53,7 @@ templates_path = ['_templates']
 source_suffix = '.rst'
 
 # The master toctree document.
-master_doc = 'index'
+master_doc = 'documentation'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
